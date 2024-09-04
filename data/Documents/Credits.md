0eb01bf3fa55ae62ad75f2768ff22f4629ad27c2
James Prevett <jp@jamespre.dev>
2024-09-04 14:56:29 -0500

    Initial

---
+++ data/Documents/Credits.md
@@ -0,0 +1,53 @@
+# Credits
+```
+________________________________________________________
+```
+  A heart felt thanks to all the devs who's tools were instrumental in making this project possible.
+  And to Dustin Brett who gave a much needed how-to guide.
+
+
+## Tutorial
+```
+________________________________________________________
+```
+- [Dustin Brett - ReactOs](https://www.youtube.com/watch?v=S-rF5rkhaJ0&list=PLM88opVjBuU7xSRoHhs3hZBz3JmHHBMMN&index=1)
+
+## Frameworks
+```
+________________________________________________________
+```
+- [Angular](https://github.com/angular/angular)
+- [TypeScript](https://github.com/microsoft/TypeScript)
+
+## Tools
+```
+________________________________________________________
+```
+- [Jest](https://github.com/facebook/jest)
+- [ESLint](https://github.com/eslint/eslint)
+- [Prettier](https://github.com/prettier/prettier)
+- [Husky](https://github.com/typicode/husky)
+
+## System Libraries
+```
+________________________________________________________
+```
+- [BrowserFS](https://github.com/jvilk/BrowserFS)
+- [ini](https://github.com/npm/ini)
+- [html-to-image](https://github.com/bubkoo/html-to-image)
+- [angular2-draggable](https://github.com/xieziyu/angular2-draggable)
+- [Vanta.js](https://github.com/tengbao/vanta)
+
+## App Libraries
+```
+________________________________________________________
+```
+- [js-dos](https://github.com/caiiiycuk/js-dos)
+- [Ruffle](https://github.com/ruffle-rs/ruffle)
+- [Marked.js](https://github.com/markedjs/marked)
+- [Monaco Editor](https://github.com/miki995/ngx-monaco-editor-v2#readme)
+- [PDF.js](https://github.com/mozilla/pdf.js)
+- [Video.js](https://github.com/videojs/video.js)
+- [Howler.js](https://github.com/goldfire/howler.js)
+- [Quill.js](https://github.com/slab/quill/)
+- [Print.js](https://github.com/crabbly/print.js)
---
+++ src/reproduction.ts
@@ -0,0 +1,78 @@
+import { basename, extname } from 'node:path';
+import { configure, Fetch, fs, Overlay } from '@zenfs/core';
+import { IndexedDB } from '@zenfs/dom';
+import { readFileSync } from 'node:fs';
+import type { IndexData } from '@zenfs/core/backends/index/index.js';
+
+let isInitialized = false;
+
+let indexData: IndexData;
+try {
+	indexData = JSON.parse(readFileSync('index.json', 'utf8'));
+} catch (e) {
+	console.error('Invalid index.');
+	process.exit();
+}
+
+async function initZenFSAsync(): Promise<void> {
+	if (isInitialized) {
+		return;
+	}
+	await configure<typeof Overlay>({
+		mounts: {
+			'/': {
+				backend: Overlay,
+				readable: { backend: Fetch, index: indexData, baseUrl: 'osdrive' },
+				writable: { backend: IndexedDB, storeName: 'fs-cache' },
+			},
+		},
+	});
+	isInitialized = true;
+}
+
+function fileName(path: string): string {
+	return `${basename(path, extname(path))}${extname(path)}`;
+}
+
+async function copyFileAsync(source: string, destinationDirectory: string): Promise<boolean> {
+	const destination = destinationDirectory + '/' + fileName(source);
+	console.log('Destination:', destination);
+	await fs.promises.copyFile(source, destination);
+	return true;
+}
+
+async function copyHandler(source: string, destination: string): Promise<boolean> {
+	const stats = await fs.promises.stat(source);
+	if (stats.isDirectory()) {
+		// ignoring directories for now
+		return true;
+	}
+	const success = await copyFileAsync(source, destination);
+	console.log(`Copy ${source} -> ${destination} succeeded: ${success}`);
+	return success;
+}
+
+declare function successHandler(destination: string): void;
+
+async function copy(source: string, destination: string): Promise<void> {
+	const folderQueue: string[] = [];
+
+	const stats = await fs.promises.stat(source);
+	if (stats.isDirectory()) {
+		folderQueue.push(source);
+	}
+
+	const success = await copyHandler(source, destination);
+	if (success) {
+		successHandler(destination);
+	}
+}
+
+async function main() {
+	console.log('Copy #1');
+	await copy('/simple.txt', '/Desktop');
+	console.log('Copy #2');
+	await copy('/simple.txt', '/Documents');
+}
+
+main();
